5a137316bd7c8159e8c495e0dbc1eb6944e1c39e
Alex Cook <alex.chef.cook@gmail.com>
2016-11-27 16:20:48 -0800

    .gitignore is now working

--- config.py
+++
@@ -1,8 +0,0 @@
-consumer_key = 'CONSUMER-KEY'
-consumer_secret = 'CONSUMER-SECRET-KEY
-access_token = 'ACCESS-KEY'
-access_secret = 'ACCESS-SECRET-KEY'
-
-connection_string = "CONN-STRING"
-table_name = 'TABLE-NAME'
-
