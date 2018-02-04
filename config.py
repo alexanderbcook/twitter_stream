2ea86470d67e79e983fa429e2460fc5e1f5ebb9d
alexanderbcook <alex.chef.cook@gmail.com>
2018-02-03 17:43:09 -0800

    cleanup of repo

--- config.py
+++
@@ -1,7 +0,0 @@
-consumer_key = 'CONSUMER_KEY'
-consumer_secret = 'CONSUMER_SECRET'
-access_token = 'ACCESS_TOKEN'
-access_secret = 'ACCESS_SECRET'
-
-connection_string = "CONN_STRING"
-table_name = 'TABLE'
