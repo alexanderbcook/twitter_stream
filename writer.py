0402bc0844150a9206032688f4dc2f45c9b5713f
alexanderbcook <alex.chef.cook@gmail.com>
2018-02-03 17:44:34 -0800

    big update, added new mode "streaming" to enhance scalability of application

---
+++ writer.py
@@ -0,0 +1,32 @@
+import redis
+import json
+import datetime
+import argparse
+import logging
+import csv
+import io
+from util import encode
+
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
+
+try:
+    redis = redis.StrictRedis(host='localhost', port=6379, db=0)
+except:
+    print "Can't connect to Redis!"
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-q",
+                    "--queries",
+                    dest="query",
+                    help="Give names of queries stored in Redis.",
+                    default='-')
+args = parser.parse_args()
+
+with io.open('data/%s.json' % args.query, 'a', encoding='utf-8') as outfile:
+    i = redis.llen(args.query)
+    logging.debug("Writing %s tweets to data/%s.csv" % (i, args.query))
+    while i > 0:
+        data = json.loads(redis.lpop(args.query)) 
+        outfile.write(unicode(json.dumps(data, ensure_ascii=False)))
+
+        i = i - 1
